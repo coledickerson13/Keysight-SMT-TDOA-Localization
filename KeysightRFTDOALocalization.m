9c5652843103eabf44ef36aab2f958cd1aaa091f
coledickerson13 <164374700+coledickerson13@users.noreply.github.com>
2024-11-01 16:33:17 -0400

    Update KeysightRFTDOALocalization.m
    more robust .csv file import due to issue with data headers

--- KeysightRFTDOALocalization.m
+++ KeysightRFTDOALocalization.m
@@ -1,16 +1,21 @@
 % Define the filename
-filename = '3.28.25.9.9MHz.csv';
-%Make sure to open CSV file and delete any spaces between column headings
+filename = '3.15.24.2Dickerson.csv';
 
-% Read the data from the CSV file
-data = readtable(filename);
+% Read the data from the CSV file without using the headers
+data = readtable(filename, 'ReadVariableNames', false);
 
-%If a location estimate is not generated, lat and long = 0. The following
-%line parses out these values.
+% Manually set the column names to match your data
+data.Properties.VariableNames = {'MeasID', 'Algorithm', 'CenterFrequency', 'SampleRate', ...
+                                 'NumSamples', 'Latitude', 'Longitude', 'Elevation', ...
+                                 'RHO', 'CEP', 'TotalSensors', 'ValidSensors', ...
+                                 'OverloadedSensors', 'SensorNames', 'Time', 'Comment'};
+
+% If a location estimate is not generated, lat and long = 0.
+% The following line parses out these values.
 rows_to_delete = data.Latitude < 1;
 data(rows_to_delete, :) = [];
 
-%reinitialize data table variables
+% Reinitialize data table variables
 Measurement_ID = data.MeasID;
 Algorithm = data.Algorithm;
 Center_Frequency = data.CenterFrequency;
@@ -28,57 +33,52 @@ Sensor_Names = data.SensorNames;
 Time = data.Time;
 Comment = data.Comment;
 
-%real coordinates for LW1
+% Real coordinates for LW1
 LW1lat = 35.72750947;
 LW1long = -78.69595810;
 
-%real coordinates for LW2 - one of three towers used to generate
-%localization estimate
+% Real coordinates for LW2 - one of three towers used to generate
+% localization estimate
 LW2lat = 35.72821305;
 LW2long = -78.70090823;
 
-%real coordinates for LW3 - one of three towers used to generate
-%localization estimate
+% Real coordinates for LW3 - one of three towers used to generate
+% localization estimate
 LW3lat = 35.72491205;
 LW3long = -78.69190014;
 
-%real coordinates for LW4 - one of three towers used to generate
-%localization estimate
+% Real coordinates for LW4 - one of three towers used to generate
+% localization estimate
 LW4lat = 35.73318358;
 LW4long = -78.6983642;
-
-%real coordinates for LW5
-% LW5lat = 35.74294142;
-% LW5long = -78.69962993;
 
 % Plot on map using Mapping Toolbox
 figure;
 geoplot(Latitude, Longitude, 'bo');
 title('Keysight Sensor Management Tool TDOA Localization of AERPAW LW1');
 hold on;
-geoplot(LW1lat, LW1long, 'r^', 'MarkerSize', 10); %LW1
-geoplot(LW2lat, LW2long, 'g^', 'MarkerSize', 10); %LW2
-geoplot(LW3lat, LW3long, 'b^', 'MarkerSize', 10); %LW3
-geoplot(LW4lat, LW4long, 'm^', 'MarkerSize', 10); %LW4
-legend('TDOA Estimated TX Coordinates', 'AERPAW LW1', 'AERPAW LW2', 'AERPAW LW3', 'AERPAW LW4')
+geoplot(LW1lat, LW1long, 'r^', 'MarkerSize', 10); % LW1
+geoplot(LW2lat, LW2long, 'g^', 'MarkerSize', 10); % LW2
+geoplot(LW3lat, LW3long, 'b^', 'MarkerSize', 10); % LW3
+geoplot(LW4lat, LW4long, 'm^', 'MarkerSize', 10); % LW4
+legend('TDOA Estimated TX Coordinates', 'AERPAW LW1', 'AERPAW LW2', 'AERPAW LW3', 'AERPAW LW4');
 hold off;
 
-%distance between LW1 and estimated coordinate points in meters
-%(localization error)
-localizationerror = deg2km(distance(LW1lat, LW1long, Latitude, Longitude))*1000;
+% Distance between LW1 and estimated coordinate points in meters
+% (localization error)
+localizationerror = deg2km(distance(LW1lat, LW1long, Latitude, Longitude)) * 1000;
 
-%average localization error
+% Average localization error
 average_error = mean(localizationerror);
 
-%normalize the first time index to 0
+% Normalize the first time index to 0
 normalized_time = Time - Time(1);
 
-%create a new figure for the second plot
+% Create a new figure for the second plot
 figure;
 
-%plot localization error against time
+% Plot localization error against time
 plot(normalized_time, localizationerror, 'bo-');
-title('SMT TDOA Localization Error vs. Time')
-xlabel('Time (Hours:Minutes:Seconds)')
-ylabel('Localization Error (meters)')
-
+title('SMT TDOA Localization Error vs. Time');
+xlabel('Time (Hours:Minutes:Seconds)');
+ylabel('Localization Error (meters)');
